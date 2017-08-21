def61bff725ab6761d5a53412f7fed3d387dde54
shakeel <shakeeljaved@hotmail.com>
2017-08-21 17:56:37 -0400

    v6

--- test.tf
+++ test.tf
@@ -27,7 +27,7 @@ resource "aws_key_pair" "builder" {
   provider = "aws.build"
   key_name   = "builder-key-${var.build_id}"
   public_key = "${file("/var/lib/jenkins/.ssh/id_rsa.pub")}"
-  # public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCnXKNmwJd9Avu1DAqCoJ+J6Oyz1y41qgElm+BJ/YrZk7Z7P3brbOsVlloPIyaJqK1yb/pEgFB0OadOv2TwXgf0fM4QMUuBTnUfJoNd+tRLkEr9bU5+XHNJ/pF3NdH26fFysWLkyWiBu5gtgalOyLNr8a+Si+c9Q6HgD9MV/NjfTmm64t8NeXtULFcypZ+GFIW6mRXJ9OrER4A81QsSMq8euMRgi4fnNvJzsdhTdz8BVbNf4Zgs6Ahuk0h1qzlrxLKMtqNo2+fEsDHboWJNkcFzCrJkMO/5XjoMsQ8mD8eZKow3Yuqy62kODmeFXeV5kMyiFHOhwvG3Hg7oRhadatWb jenkins@ip-10-168-1-133"
+  # public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDH4l0SJHhsm4IVQ8j0JeIy8XKfYaVpUj+VY0C1d8r9R/RAus20UrF2bBshiR9hWt3d8P411WtfHteKHSzhFM2E4I6+K4h0Ib1Rqf9rnXYr+J8UeI8JqvK7tJvPm3loAD9kc+y+q+Ow5oq1ukAJbR5wHHBWfyE+Rop2i4FT0Nf8IEtYn/TtKkd3SS1FBhOn4mfcnlyyq+24DbPrgHDf6KYwnxLLRBUVs/pV0A1EQ5H377RlC0c3Qm7C7LBLxh2sTV/Utud3ug8dIlujw/gV/g9INQdCl4uvxCZ0KIeBAk/aW+AJEfmBXj8aLFC64eF9+LvXqBT+5YCnG1Sej99sdE1Z ec2-user@ip-172-31-16-30.ec2.internal"
 }
 
 resource "aws_security_group" "aws_sec" {
